1fbf4735733810ba4b6254eeb63a718b035e9a21
Beyonder69 <114392649+Beyonder69@users.noreply.github.com>
2022-11-29 00:24:02 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -167,11 +167,6 @@ HELP_STRINGS = """
 GROUP_START_IMG = (
     "https://graph.org/file/b1faed61b726848ae6cf2.jpg"
     
-    
-    
-    
-)
-
 DONATE_STRING = """❂ I'm Free for Everyone ❂"""
 
 IMPORTED = {}
