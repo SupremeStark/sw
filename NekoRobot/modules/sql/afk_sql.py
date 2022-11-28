8398dfa0dba2587312761f05a0a2566a625d143e
Beyonder69 <114392649+Beyonder69@users.noreply.github.com>
2022-11-28 23:35:48 +0530

    Update afk_sql.py

--- NekoRobot/modules/sql/afk_sql.py
+++ NekoRobot/modules/sql/afk_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Boolean, BigInteger
 
-from AsukaRobot.modules.sql import BASE, SESSION
+from Scarlet witch bot.modules.sql import BASE, SESSION
 
 
 class AFK(BASE):
