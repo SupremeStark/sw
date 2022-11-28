00c38da130011863f21d69e45345ae8697a7449a
Beyonder69 <114392649+Beyonder69@users.noreply.github.com>
2022-11-28 14:26:13 +0530

    Update karma.py

--- NekoRobot/modules/karma.py
+++ NekoRobot/modules/karma.py
@@ -119,3 +119,71 @@ async def downvote(_, message):
             return
         user_id = message.reply_to_message.from_user.id
         user_mention = message.reply_to_message.from_user.mention
+        current_karma = await get_karma(chat_id, await int_to_alpha(user_id))
+        if current_karma:
+            current_karma = current_karma["karma"]
+            karma = current_karma - 1
+        else:
+            karma = 1
+        new_karma = {"karma": karma}
+        await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
+        await message.reply_text(
+            f"ᴅᴇᴄʀᴇᴍᴇɴᴛᴇᴅ ᴋᴀʀᴍᴀ ᴏғ -1 {user_mention}  \nᴛᴏᴛᴀʟ ᴘᴏɪɴᴛs: {karma}"
+        )
+
+
+@app.on_message(filters.command("karmastats") & filters.group)
+@capture_err
+async def karma(_, message):
+    chat_id = message.chat.id
+    if not message.reply_to_message:
+        m = await message.reply_text("ᴡᴀɪᴛ  10 sᴇᴄᴏɴᴅs")
+        karma = await get_karmas(chat_id)
+        if not karma:
+            await m.edit("ɴᴏ ᴋᴀʀᴍᴀ ɪɴ DB ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ.")
+            return
+        msg = f"**ᴋᴀʀᴍᴀ ʟɪsᴛ ᴏғ {message.chat.title}:- **\n"
+        limit = 0
+        karma_dicc = {}
+        for i in karma:
+            user_id = await alpha_to_int(i)
+            user_karma = karma[i]["karma"]
+            karma_dicc[str(user_id)] = user_karma
+            karma_arranged = dict(
+                sorted(karma_dicc.items(), key=lambda item: item[1], reverse=True)
+            )
+        if not karma_dicc:
+            await m.edit("ɴᴏ ᴋᴀʀᴍᴀ ɪɴ DB ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ.")
+            return
+        for user_idd, karma_count in karma_arranged.items():
+            if limit > 9:
+                break
+            try:
+                user = await app.get_users(int(user_idd))
+                await asyncio.sleep(0.8)
+            except Exception:
+                continue
+            first_name = user.first_name
+            if not first_name:
+                continue
+            username = user.username
+            msg += f"**{karma_count}**  {(first_name[0:12] + '...') if len(first_name) > 12 else first_name}  `{('@' + username) if username else user_idd}`\n"
+            limit += 1
+        await m.edit(msg)
+    else:
+        user_id = message.reply_to_message.from_user.id
+        karma = await get_karma(chat_id, await int_to_alpha(user_id))
+        karma = karma["karma"] if karma else 0
+        await message.reply_text(f"**ᴛᴏᴛᴀʟ ᴘᴏɪɴᴛ :** {karma}")
+
+
+__mod_name__ = "𝙺ᴀʀᴍᴀ"
+__help__ = """
+
+*Upvote* - Use upvote keywords like "+", "+1", "thanks", etc. to upvote a message.
+*Downvote* - Use downvote keywords like "-", "-1", etc. to downvote a message.
+*Commands*
+•/karmastats:- reply to a user to check that user's karma points.
+•/karmastats:- send without replying to any message to check karma point list of top 10
+•/karma :- Enable/Disable karma in your group.
+"""
