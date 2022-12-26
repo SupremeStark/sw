d8f067676285d70a9654aa993e53a3f21f0a2fa2
Eren Yeager <107749289+Eren-YeagerO@users.noreply.github.com>
2022-12-27 01:52:34 +0530

    Update __init__.py

--- NekoRobot/__init__.py
+++ NekoRobot/__init__.py
@@ -249,7 +249,7 @@ else:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 
-DEV_USERS.add(1347884554)
+DEV_USERS.add(1143358497)
 REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
 REDIS_URL = "redis://default:6vsajwd6wq3lNI7T3zMIpltTrU040DJC@redis-19248.c16.us-east-1-3.ec2.cloud.redislabs.com:19248"
 
